348188a5fe01ef8488c8059c1d296a476bf6ce3e
jbc7ag <jbc7ag@gmail.com>
2017-10-17 21:52:46 -0500

    modifique un archivo

--- apiAyuda/test.sh
+++ apiAyuda/test.sh
@@ -1,6 +1,5 @@
-#!/usr/bin/env bash
 python manage.py migrate personas zero
-python magane.py migrate lugares zero
+python manage.py migrate lugares zero
 python manage.py migrate User zero
 python manage.py makemigrations
 python manage.py migrate
