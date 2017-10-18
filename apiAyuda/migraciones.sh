ed7c6ae5b34bfb66d8a34bdee618421d156d29da
jbc7ag <jbc7ag@gmail.com>
2017-10-17 21:51:26 -0500

    modifique migrations.sh

--- apiAyuda/migraciones.sh
+++ apiAyuda/migraciones.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 python manage.py migrate personas zero
-python magane.py migrate lugares zero
+python manage.py migrate lugares zero
 python manage.py migrate User zero
 python manage.py makemigrations
 python manage.py migrate
+
 gunicorn --bind 0.0.0.0:8000 apiAyuda.wsgi:aplication
